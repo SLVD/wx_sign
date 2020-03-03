4ea2f83f108ad9323cc8be641bd9f558b7ddba69
su <su@localhost.localdomain>
2020-03-02 19:55:08 -0500

    su

--- wx.php
+++ wx.php
@@ -5,13 +5,15 @@
         $nonce = $_GET["nonce"];
         
         $token = "sudidi";
+        $echostr = $_GET["echostr"];
+        
         $tmpArr = array($token, $timestamp, $nonce);
         sort($tmpArr, SORT_STRING);
         $tmpStr = implode( $tmpArr );
         $sign = sha1( $tmpStr );
         
         if( $sign == $signature ){
-            return true;
+            echo $echostr;
         }else{
             return false;
         }
